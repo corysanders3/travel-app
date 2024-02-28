2e4983a818acdc2e8b9c8cfdce40a914ee5776af
Cory Sanders <corysanders3@yahoo.com>
2024-02-28 16:50:19 -0700

    Add functionality to get a users trips and total cost.

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,1 +1,2 @@
 // this is where domUpdates will go
+import { fetchTraveler } from './fetchRequests.js';
--- src/travelerInfo.js
+++ src/travelerInfo.js
@@ -1,3 +1,41 @@
 // this is where basic functionality will go 
 // to then be used to update the dom with
 // these functions are the ones that will be tested
+
+function getTravelerTrips(allTrips, id) {
+    if(id) {
+        let myTrips = allTrips.trips.filter((trip) => {
+            return trip.userID === id;
+        })
+        myTrips.forEach((trip) => {
+            trip.date = new Date(trip.date);
+        })
+        myTrips.sort((a, b) => {
+            return b.date - a.date;
+        })
+        myTrips.forEach((trip) => {
+            trip.date = trip.date.toISOString().split('T')[0].replaceAll('-', '/');
+        })
+        return myTrips;
+    } else {
+        return 'This User ID does not exist.';
+    }
+}
+
+function getTotalCost(myTrips, allDest, status) {
+    let approvedTrips = myTrips.filter((trip) => {
+        return trip.status === 'approved' || status;
+    })
+    let totalCost = approvedTrips.reduce((total, trip) => {
+        allDest.destinations.forEach((dest) => {
+            if(trip.destinationID === dest.id) {
+                total += (dest.estimatedFlightCostPerPerson * trip.travelers);
+                total += (dest.estimatedLodgingCostPerDay * trip.duration);
+            }
+        })
+        return total;
+    }, 0);
+    return (totalCost * 1.1)
+}
+
+export { getTravelerTrips, getTotalCost };
