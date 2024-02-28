62276df83320fc084abb2200fca3fe85d593a7a8
Cory Sanders <corysanders3@yahoo.com>
2024-02-27 20:23:42 -0700

    Add files to use for specified reasons.

---
+++ src/travelerInfo.js
@@ -0,0 +1,3 @@
+// this is where basic functionality will go 
+// to then be used to update the dom with
+// these functions are the ones that will be tested
---
+++ test/traveler-test.js
@@ -0,0 +1,1 @@
+// this is where my unit tests will go
